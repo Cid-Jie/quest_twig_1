aca5f79c9b2d4ea1af4f9d638143afab78cef264
Cid-Jie <pcidjie@gmail.com>
2022-04-03 13:55:10 +0200

    add files to instrument

--- public/index.php
+++ public/index.php
@@ -7,3 +7,14 @@ require_once __DIR__ . '/../config/twig.php';
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+
+echo $twig->render('home.html.twig', array(
+    'name' => 'Michel',
+    'age' => 10,
+
+    'users' => array(
+        array('name' => 'Max', 'age' => 22),
+        array('name' => 'James', 'age' => 35),
+        array('name' => 'Billy', 'age' => 56)
+    )
+));
---
+++ public/products.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', array(
+    'products' => $products
+));
